b6aabcae15fccf90125ff2fc4194486bea8d0f8a
dreclor <93927337+dreclor@users.noreply.github.com>
2021-11-08 09:28:19 -0500

    Update MetaCoin.sol
    yur

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -14,7 +14,7 @@ contract MetaCoin {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     constructor() public {
-        balances[tx.origin] = 10000;
+        balances[tx.origin] = 30000;
     }
 
     function sendCoin(address receiver, uint256 amount)
