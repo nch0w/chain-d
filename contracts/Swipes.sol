2e845fe25f9114be09089ae2e50a70a58927137f
Neil Chowdhury <neil.chowdhury@outlook.com>
2021-11-17 07:40:53 -0500

    add swipes contract

--- contracts/Swipes.sol
+++ contracts/Swipes.sol
@@ -1,5 +1,26 @@
 pragma solidity >=0.4.25;
 
 contract Matching {
-    mapping(address => address[100]) public swipes;
+    struct Swipe {
+        bytes encryptedAddress;
+        uint256 timestamp;
+        uint256 fee;
+    }
+
+    mapping(address => Swipe[]) public swipes;
+
+    constructor() public payable {}
+
+    function() external payable {
+        if (msg.value >= 1000000000) {
+            bytes memory matchAddress = msg.data;
+            swipes[msg.sender].push(
+                Swipe(matchAddress, block.timestamp, msg.value)
+            );
+        }
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
 }
